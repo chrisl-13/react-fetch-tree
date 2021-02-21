6227f28d7c1a5beae44365bd4774ef0466e30f0f
Chris Lung <chrislung@Chriss-MacBook-Pro.local>
2021-02-21 16:11:12 -0500

    testing

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
-import React, { useContext } from "react";
+import React, { useContext } from 'react';
+import rftParser from './src/parser.js';
 
 
+// This component attaches itself to the root component and parses through all dependency files
+// Returns an object to help visualize our ReactFetchTree in the Chrome Extension
+const ReactFetchTree = ({ rootId }) => {
+  
+}
 
+export default ReactFetchTree
